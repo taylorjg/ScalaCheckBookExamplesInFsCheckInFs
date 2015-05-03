8fba7001a39355931cc967bcb666c527350d8c07
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-03 17:51:07 +0100

    Tweaks to runLengthEnc

--- ScalaCheckBookExamplesInFsCheckInFs/Chapter4_RunLengthEnc.fs
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter4_RunLengthEnc.fs
@@ -1,14 +1,14 @@
 ﻿module ScalaCheckBookExamplesInFsCheckInFs.Chapter4_RunLengthEnc
 
 let runLengthEnc (xs: list<'a>): list<int * 'a> =
-    let rec loop (xs: list<'a>) (pe: option<int * 'a>): seq<int * 'a> =
+    let rec loop (xs: list<'a>) (currTuple: option<int * 'a>): seq<int * 'a> =
         seq {
-            match (xs, pe) with
-                | ([], None) -> yield! Seq.empty
-                | ([], Some(t)) -> yield t
-                | (x::rest, None) -> yield! loop rest (Some(1, x))
-                | (x1::rest, Some((n, x2))) when x1 = x2 -> yield! loop rest (Some(n + 1, x2))
-                | (x::rest, Some(t)) -> yield t; yield! loop rest (Some(1, x))
+            match xs, currTuple with
+                | [], None -> yield! Seq.empty
+                | [], Some(t) -> yield t
+                | x::rest, None -> yield! loop rest (Some (1, x))
+                | x1::rest, Some(n, x2) when x1 = x2 -> yield! loop rest (Some (n + 1, x2))
+                | x::rest, Some(t) -> yield t; yield! loop rest (Some (1, x))
         }
     loop xs None |> Seq.toList
 
