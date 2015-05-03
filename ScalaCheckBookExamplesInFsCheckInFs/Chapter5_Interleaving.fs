0b45c9f3969c66a7cc3623aec9052df48e794acb
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-03 22:15:31 +0100

    Added the interleaving example from chapter 5

---
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter5_Interleaving.fs
@@ -0,0 +1,6 @@
+﻿module ScalaCheckBookExamplesInFsCheckInFs.Chapter5_Interleaving
+
+let rec interleave (xs: list<'a>) (ys: list<'a>): list<'a> =
+    if xs.IsEmpty then ys
+    else if ys.IsEmpty then xs
+    else xs.Head :: ys.Head :: interleave xs.Tail ys.Tail
---
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter5_InterleavingTests.fs
@@ -0,0 +1,20 @@
+﻿module ScalaCheckBookExamplesInFsCheckInFs.Chapter5_InterleavingTests
+
+open FsCheck
+open NUnit.Framework
+open Chapter5_Interleaving
+open System.Linq
+
+let config = Config.VerboseThrowOnFailure
+
+let property (xs: list<int>) (ys: list<int>): Property =
+    let res = interleave xs ys
+    let is = [0..((min xs.Length ys.Length) - 1)]
+    let p1 = "length" @| (xs.Length + ys.Length = res.Length)
+    let p2 = "zip xs" @| (xs = List.map (fun i -> List.nth res (2*i)) is @ (Enumerable.Skip(res, 2*ys.Length) |> Seq.toList))
+    let p3 = "zip ys" @| (ys = List.map (fun i -> List.nth res (2*i+1)) is @ (Enumerable.Skip(res, 2*xs.Length) |> Seq.toList))
+    p1 .&. p2 .&. p3
+
+[<Test>]
+let interleavingTest() =
+    Check.One (config, property)
