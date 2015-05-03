a89456aed0381f0f9a620af6b7cfefebd71532df
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-03 10:28:02 +0100

    Split the code under test and the test into separate modules

--- ScalaCheckBookExamplesInFsCheckInFs/Chapter4_RunLengthEnc.fs
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter4_RunLengthEnc.fs
@@ -1,13 +1,5 @@
 ﻿module ScalaCheckBookExamplesInFsCheckInFs.Chapter4_RunLengthEnc
 
-open FsCheck
-open FsCheckUtils
-open Gen
-open Arb
-open NUnit.Framework
-
-let config = Config.VerboseThrowOnFailure
-
 let runLengthEnc (xs: list<'a>): list<int * 'a> =
     let rec loop (xs: list<'a>) (acc: list<int * 'a>) (pe: option<int * 'a>): list<int * 'a> =
         match xs with
@@ -26,24 +18,3 @@ let runLengthEnc (xs: list<'a>): list<int * 'a> =
 let runLengthDec (r: list<int * 'a>): list<'a> =
     List.collect (fun t -> t ||> List.replicate) r
 
-let genOutput: Gen<list<int * char>> =
-    let rleItem: Gen<int * char> = gen {
-        let! n = choose (1, 20)
-        let! c = GenExtensions.AlphaNumChar
-        return (n, c)
-    }
-    let rec rleList (size: int): Gen<list<int * char>> =
-        if (size <= 1) then map (fun x -> [x]) rleItem
-        else gen {
-            let! tail = rleList (size - 1)
-            let (_, c1) = List.head tail
-            let! head = suchThat (fun (_, c2) -> c2 <> c1) rleItem
-            return head :: tail
-        }
-    sized rleList
-
-[<Test>]
-let runLengthEncTest() =
-    let arb = fromGen genOutput
-    let property = Prop.forAll arb (fun r -> runLengthEnc(runLengthDec(r)) = r)
-    Check.One (config, property)
---
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter4_RunLengthEncTests.fs
@@ -0,0 +1,32 @@
+﻿module ScalaCheckBookExamplesInFsCheckInFs.Chapter4_RunLengthEncTests
+
+open FsCheck
+open FsCheckUtils
+open Gen
+open Arb
+open NUnit.Framework
+open Chapter4_RunLengthEnc
+
+let config = Config.VerboseThrowOnFailure
+
+let genOutput: Gen<list<int * char>> =
+    let rleItem: Gen<int * char> = gen {
+        let! n = choose (1, 20)
+        let! c = GenExtensions.AlphaNumChar
+        return (n, c)
+    }
+    let rec rleList (size: int): Gen<list<int * char>> =
+        if (size <= 1) then map (fun x -> [x]) rleItem
+        else gen {
+            let! tail = rleList (size - 1)
+            let (_, c1) = List.head tail
+            let! head = suchThat (fun (_, c2) -> c2 <> c1) rleItem
+            return head :: tail
+        }
+    sized rleList
+
+[<Test>]
+let runLengthEncTest() =
+    let arb = fromGen genOutput
+    let property = Prop.forAll arb (fun r -> runLengthEnc(runLengthDec(r)) = r)
+    Check.One (config, property)
