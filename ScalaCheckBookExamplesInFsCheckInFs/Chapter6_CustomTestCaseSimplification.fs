8cc20149c356fe40c9768a19b0b4033ae58bdfff
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-07-08 13:35:26 +0100

    More work on the custom test case simplification example

--- ScalaCheckBookExamplesInFsCheckInFs/Chapter6_CustomTestCaseSimplification.fs
+++ ScalaCheckBookExamplesInFsCheckInFs/Chapter6_CustomTestCaseSimplification.fs
@@ -16,6 +16,16 @@ let rec eval expr =
         | Add (e1, e2) -> eval e1 + eval e2
         | Mul (e1, e2) -> eval e1 * eval e2
 
+let rewrite e = 
+    match e with
+         Add (e1, e2) when e1 = e2 -> Mul (Const 2, e1)
+        | Mul (Const 0, _) -> Const 0
+        // "Add (Const 1, e) -> e" is a bug. Use it instead of
+        // "Add (Const 0, e) -> e" to demonstrate shrinking.
+        // | Add (Const 1, e) -> e
+        | Add (Const 0, e) -> e
+        | _ -> e
+
 #nowarn "40"
 let rec genExpr =
     sized <| fun sz ->
@@ -42,7 +52,20 @@ and genMul =
         return Mul (e1, e2)
     }
 
+type MyGenerators =
+  static member Expression() =
+    { new Arbitrary<Expression>() with
+          override x.Generator = genExpr
+          // That's interesting. I haven't implemented shrinking but it still shrinks anyway!
+          override x.Shrinker t = Seq.empty
+    }
+
+Arb.register<MyGenerators>() |> ignore
+
+let propRewrite expr =
+    eval (rewrite expr) = eval expr
+
 [<Test>]
-let doSample() =
-    let xs = sample 10 10 genExpr
-    List.map (printfn "%A") xs |> ignore
+let testRewrite() =
+    let config = { Config.QuickThrowOnFailure with MaxTest = 1000 }
+    Check.One (config, propRewrite)
